89923984bb536925726840029277347dd1207147
Anjlovrgse <anjalovarghese4@gmail.com>
2025-01-23 23:23:42 +0530

    Updated

--- FCFS.c
+++ FCFS.c
@@ -12,35 +12,37 @@ int main()
     scanf("%d", &n);
 
     printf("Enter %d process IDs: ", n);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         scanf("%d", &p[i]);
     }
 
     printf("Enter %d arrival times: ", n);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         scanf("%d", &at[i]);
     }
 
     printf("Enter %d burst times: ", n);
-    for (i = 0; i < n; i++) {
+    for (i = 0; i < n; i++) 
+    {
         scanf("%d", &bt[i]);
     }
 
-    // Sort based on arrival time (using Bubble Sort)
     for (i = 0; i < n - 1; i++) {
         for (j = 0; j < n - i - 1; j++) {
             if (at[j] > at[j + 1]) {
-                // Swap process IDs
+               
                 temp = p[j];
                 p[j] = p[j + 1];
                 p[j + 1] = temp;
 
-                // Swap arrival times
+               
                 temp = at[j];
                 at[j] = at[j + 1];
                 at[j + 1] = temp;
 
-                // Swap burst times
+                
                 temp = bt[j];
                 bt[j] = bt[j + 1];
                 bt[j + 1] = temp;
@@ -48,7 +50,6 @@ int main()
         }
     }
 
-    // Compute Completion Time
     ct[0] = at[0] + bt[0];
     for (i = 1; i < n; i++) {
         if (ct[i - 1] < at[i]) {
@@ -58,18 +59,17 @@ int main()
         }
     }
 
-    // Calculate Turnaround Time and Waiting Time
     for (i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i]; // Turnaround Time
-        wt[i] = tat[i] - bt[i]; // Waiting Time
-        atat += tat[i];         // Accumulate total TAT
-        awt += wt[i];           // Accumulate total WT
+        tat[i] = ct[i] - at[i]; 
+        wt[i] = tat[i] - bt[i]; 
+        atat += tat[i];         
+        awt += wt[i];          
     }
 
-    atat /= n; // Average Turnaround Time
-    awt /= n;  // Average Waiting Time
+    atat /= n; 
+    awt /= n;  
 
-    // Display results
+  
     printf("\nPID\tAT\tBT\tCT\tTAT\tWT\n");
     for (i = 0; i < n; i++) {
         printf("%d\t%d\t%d\t%d\t%d\t%d\n", p[i], at[i], bt[i], ct[i], tat[i], wt[i]);
